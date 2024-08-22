ef6b33e1f7dde8d3a2404279ac0d86a2f17b5268
MaLingJi <a385500062@gmail.com>
2024-08-22 15:59:24 +0800

    首頁穿搭分享測試

---
+++ src/components/share/ShareCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>Share</h2>
+    <div class="column">
+        <div class="ts-box">
+            <div class="ts-image">
+                <img src="../../../public/No_image.png" />
+            </div>
+            <div class="ts-divider"></div>
+            <div class="ts-content">{{ post.contentText }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps(["post"]);
+</script>
+
+<style></style>
