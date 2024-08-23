da574a3a58ead9909b733abfdf2a451d4a8f0e40
yu <s7919085@gmail.com>
2024-08-23 16:12:38 +0800

    商品分類

--- src/views/Forbidden.vue
+++ src/views/Forbidden.vue
@@ -1,5 +1,13 @@
-<template>
-    <h3>403：沒有權限使用此網頁</h3>
+    <template>
+  <n-result
+    status="403"
+    title="403 禁止訪問"
+    description="沒有權限使用此網頁"
+  >
+    <template #footer>
+      <n-button>放轻松</n-button>
+    </template>
+  </n-result>
 </template>
     
 <script setup>
