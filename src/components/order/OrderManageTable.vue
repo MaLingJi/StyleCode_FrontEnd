63331e5485b3733405ce2382e183ec17c683db00
MaLingJi <a385500062@gmail.com>
2024-09-16 10:56:46 +0800

    table height

--- src/components/order/OrderManageTable.vue
+++ src/components/order/OrderManageTable.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <div class="table-container">
-            <table class="ts-table "  style="height: 479px; width: 100%;">
+        <div class="table-container" style="height: 479px; width: 100%;">
+            <table class="ts-table " >
                 <thead>
                     <tr>
                         <th>名稱</th>
